41f46552cb173d6587b4cf91e9e094d929313949
SZEDO-Scripts <szedoscripts01@gmail.com>
2025-06-30 14:13:00 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+
+
+
+author 'SZEDO'
+description 'Modern ProgressBar By SZEDO'
+version '1.0.0'
+
+client_script 'client.lua'
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/script.js'
+}
